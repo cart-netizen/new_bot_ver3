63a90f965ae5dadac9d7b764ffbad78fa086f97f
1q <us89@mail.ru>
2025-10-09 10:22:48 +0700

    1

--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -1,13 +1,2 @@
-declare global {
-  namespace React {
-    type ReactNode =
-      | string
-      | number
-      | boolean
-      | null
-      | undefined
-      | React.ReactElement
-      | React.ReactFragment
-      | React.ReactPortal;
-  }
-}
+
+/// <reference types="vite/client" />
